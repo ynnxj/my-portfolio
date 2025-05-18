b8285627aa5daeee6a796ee060ece27fde1ac245
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-18 17:55:49 +0200

    ✨ feat(views): add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,258 @@
-<script setup lang="ts">
-import TheHeader from '../components/TheHeader.vue'
-import TheFooter from '@/components/TheFooter.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheHeader />
+    <section class="about-section" id="about-section">
+      <img class="portrait" src="/src/assets/portraits/portrait-1.PNG" />
 
-    <section class="about-section">
-      <h2>About me</h2>
-      <p>Some text about me here.</p>
+      <div class="wrapper">
+        <article class="about-text">
+          <h2>About me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum. I’m passionate about crafting clean, user-friendly
+            interfaces, and I’ve taken a UX design course to better understand how users think and
+            interact with digital products.
+          </p>
+        </article>
+
+        <article class="technical-text">
+          <span>
+            <h3>Languages</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Sass</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+            </ul>
+          </span>
+
+          <span>
+            <h3>Frameworks</h3>
+            <ul>
+              <li>Vue</li>
+              <li>Express</li>
+            </ul>
+          </span>
+
+          <span>
+            <h3>Tools</h3>
+            <ul>
+              <li>GitHub</li>
+              <li>Figma</li>
+              <li>VS Code</li>
+            </ul>
+          </span>
+        </article>
+
+        <article class="backend-text">
+          <h3>Backend</h3>
+          <p>
+            Familiar with basic backend tools like MySQL, MongoDB, Compass, and Insomnia for API
+            testing.
+          </p>
+        </article>
+      </div>
     </section>
 
+    <h2>Projects</h2>
     <section class="project-section">
-      <h2>Projects</h2>
-      <p>Some text about my projects.</p>
+      <article class="project">
+        <img src="/src/assets/projects/lumina-preview.png" />
+        <div>
+          <h3>Lumina</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+          <button>demo</button>
+          <button>github</button>
+        </div>
+      </article>
+
+      <article class="project">
+        <img src="/src/assets/projects/bookhaus-preview.png" />
+        <div>
+          <h3>Book Haus</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+          <button>demo</button>
+          <button>github</button>
+        </div>
+      </article>
+
+      <article class="project">
+        <img src="/src/assets/projects/webshop-preview.png" />
+        <div>
+          <h3>Webshop</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+          <button>demo</button>
+          <button>github</button>
+        </div>
+      </article>
+
+      <article class="project">
+        <img src="/src/assets/projects/quiz-preview.png" />
+        <div>
+          <h3>Quiz</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+          <button>demo</button>
+          <button>github</button>
+        </div>
+      </article>
+
+      <article class="project">
+        <img src="/src/assets/projects/nemo-preview.png" />
+        <div>
+          <h3>Nemo (UX)</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+          <button>demo</button>
+          <button>github</button>
+        </div>
+      </article>
     </section>
 
     <section class="creative-work-section">
-      <h2>Creative work</h2>
-      <p>Some text about creative work.</p>
-    </section>
+      <article class="text-section">
+        <h2>Creative work</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </article>
 
-    <TheFooter />
+      <article class="artwork-section">
+        <img class="artwork" src="/src/assets/creative-work/paranoid-orange.JPG" />
+        <img class="artwork" src="/src/assets/creative-work/sad-blue.JPG" />
+        <img class="artwork" src="/src/assets/creative-work/tears-white.JPG" />
+        <img class="artwork" src="/src/assets/creative-work/self-portrait.JPG" />
+      </article>
+    </section>
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  h2 {
+    font-size: 2em;
+    margin-top: 5rem;
+  }
+  h3 {
+    font-size: 1.5em;
+  }
+  p {
+    font-size: 1em;
+  }
+  img {
+    width: 500px;
+  }
+  margin: 125px;
+}
+
+.about-section {
+  display: flex;
+  align-items: flex-start;
+  gap: 5rem;
+  border-bottom: 1px solid white;
+  border-top: 1px solid white;
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .technical-text {
+    display: flex;
+    flex-direction: row;
+    gap: 18rem;
+  }
+}
+
+.project-section {
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  border-bottom: 1px solid white;
+
+  .project {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5rem;
+
+    &:last-of-type {
+      margin-bottom: 7rem;
+    }
+
+    h3 {
+      margin: 0;
+    }
+
+    button:first-of-type {
+      margin-right: 1rem;
+    }
+  }
+}
+
+.creative-work-section {
+  display: flex;
+  align-items: flex-start;
+  gap: 5rem;
+  margin: 7rem 0;
+  text-align: center;
+  h2 {
+    margin: 0;
+  }
+}
+
+.artwork-section {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+
+  .artwork {
+    width: 200px;
+  }
+}
+</style>
