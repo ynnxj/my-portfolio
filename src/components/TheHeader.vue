d1a5cf8a2bda2b4892ed89ec25f8b2a2386d90c3
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-19 19:59:18 +0200

    ✨ feat(header): add header component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,42 +1,11 @@
-<script setup lang="ts">
-window.addEventListener('scroll', () => {
-  const navbar = document.getElementById('navbar')
-  const header = document.getElementById('header')
-
-  if (!navbar || !header) return
-
-  const headerHeight = header.offsetHeight
-
-  if (window.scrollY > headerHeight) {
-    navbar.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
-  }
-})
-
-const scrollToSection = () => {
-  const el = document.getElementById('about-section')
-  if (el) {
-    el.scrollIntoView({ behavior: 'smooth' })
-  }
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <nav id="navbar">
-    <img src="./icons/star-decal.svg" />
-    <span>
-      <RouterLink class="nav-link" to="/">home</RouterLink>
-      <RouterLink class="nav-link" to="/" @click.prevent="scrollToSection">about</RouterLink>
-      <RouterLink class="nav-link" to="/contact">contact</RouterLink>
-    </span>
-  </nav>
-
   <div class="header" id="header">
     <!-- Header Background Image -->
     <img
       class="header-img"
-      src="/src/assets/header-background.jpg"
+      src="/src/assets/backgrounds/header-background.jpg"
       alt="A background with blurry shapes in warm colors"
       loading="lazy"
     />
@@ -61,7 +30,7 @@ const scrollToSection = () => {
     <div class="right-text">
       <div>
         <p>Jennifer</p>
-        <p>NILSSON</p>
+        <p>JENNIFER</p>
       </div>
       <div>
         <p>Nilsson</p>
@@ -74,75 +43,47 @@ const scrollToSection = () => {
 <style lang="scss" scoped>
 .header {
   height: 100vh;
-  width: 100%;
+  width: 100vw;
   overflow: hidden;
   display: grid;
   grid-template-columns: 1fr auto 1fr;
   align-items: center;
+  position: relative;
+
+  .header-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    object-position: 0 50%;
+    z-index: -1;
+  }
 
   .left-text {
-    justify-self: start;
+    justify-self: center;
     display: flex;
+    position: relative;
+    top: 10rem;
     flex-direction: column;
-    padding: 100px;
-    margin-top: 400px;
     gap: 10px;
   }
 
   .right-text {
+    justify-self: center;
+    text-align: center;
+    position: relative;
+    bottom: 10rem;
     display: flex;
     flex-direction: column;
-    justify-self: end;
-    margin-bottom: 400px;
-    padding: 100px;
-  }
-
-  .header-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: 0 50%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
+    font-size: 1.5rem;
   }
 
   .logo {
-    width: 1000px;
-    margin-bottom: 100px;
+    width: 70rem;
+    justify-self: center;
+    margin: 0 auto;
   }
-}
-
-nav {
-  position: fixed;
-  width: 100vw;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 50px;
-  transition: background-color 0.3s ease;
-
-  &.scrolled {
-    background-color: rgba(0, 0, 0, 0.8);
-  }
-
-  img {
-    width: 50px;
-    margin: 15px 30px;
-  }
-
-  span {
-    .nav-link {
-      color: var(--primary-color);
-      margin-right: 80px;
-    }
-  }
-}
-
-.text {
-  position: relative;
 }
 </style>
