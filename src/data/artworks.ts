59f50ffe03300644148942dab034e44a20aef020
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-19 20:02:27 +0200

    📇 feat(data): create object arrays for projects and art

---
+++ src/data/artworks.ts
@@ -0,0 +1,22 @@
+export const artworks = [
+  {
+    id: 1,
+    image: '/src/assets/creative-work/paranoid-orange.JPG',
+    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 2,
+    image: '/src/assets/creative-work/sad-blue.JPG',
+    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    image: '/src/assets/creative-work/tears-white.JPG',
+    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 4,
+    image: '/src/assets/creative-work/self-portrait.JPG',
+    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+]
---
+++ src/data/projects.ts
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'Lumina',
+    image: '/src/assets/projects/lumina-preview.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    demoLink: '#',
+    githubLink: '#',
+  },
+  {
+    id: 2,
+    title: 'Book Haus',
+    image: '/src/assets/projects/bookhaus-preview.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    demoLink: '#',
+    githubLink: '#',
+  },
+  {
+    id: 3,
+    title: 'Webshop',
+    image: '/src/assets/projects/webshop-preview.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    demoLink: '#',
+    githubLink: '#',
+  },
+  {
+    id: 4,
+    title: 'Quiz',
+    image: '/src/assets/projects/quiz-preview.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    demoLink: '#',
+    githubLink: '#',
+  },
+  {
+    id: 5,
+    title: 'Nemo (UX)',
+    image: '/src/assets/projects/nemo-preview.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    demoLink: '#',
+    githubLink: '#',
+  },
+]
