fcc34e1c7846aaa336e4c87a4d3790ef3e9efc57
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-19 20:01:03 +0200

    ♻️ refactor(App.vue): update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import TheHeader from './components/TheHeader.vue'
+import TheNavbar from './components/TheNavbar.vue'
 import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
-  <TheHeader />
-  <RouterView />
-  <TheFooter />
+  <div id="app">
+    <TheNavbar />
+    <RouterView />
+    <TheFooter />
+  </div>
 </template>
