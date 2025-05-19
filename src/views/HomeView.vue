8bee277499a590ed47153ed917271502653242a0
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-19 20:03:16 +0200

    ✨ feat(views): update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from '../components/TheHeader.vue'
+import { projects } from '@/data/projects'
+import { artworks } from '@/data/artworks'
+import { onMounted } from 'vue'
+import gsap from 'gsap'
+
+onMounted(() => {
+  gsap.to('.portrait', {
+    rotation: 3,
+    duration: 0.8,
+    yoyo: true,
+    repeat: -1,
+    ease: 'steps(1)',
+    transformOrigin: '50% 100%',
+  })
+  gsap.set('.portrait', { rotation: -3 })
+})
+
+function openLink(url: string) {
+  if (url) window.open(url, '_blank')
+}
+</script>
 
 <template>
   <main>
+    <TheHeader />
     <section class="about-section" id="about-section">
       <img class="portrait" src="/src/assets/portraits/portrait-1.PNG" />
+      <img class="star-graphic" src="../components/icons/bigger-star.png" />
+      <img class="tiny-star-graphic" src="../components/icons/tiny-star.png" />
 
       <div class="wrapper">
         <article class="about-text">
@@ -19,7 +44,6 @@
             interact with digital products.
           </p>
         </article>
-
         <article class="technical-text">
           <span>
             <h3>Languages</h3>
@@ -58,94 +82,23 @@
           </p>
         </article>
       </div>
+      <img class="lines-graphic" src="../components/icons/lines.svg" />
     </section>
 
-    <h2>Projects</h2>
     <section class="project-section">
-      <article class="project">
-        <img src="/src/assets/projects/lumina-preview.png" />
+      <h2>Projects</h2>
+
+      <article v-for="project in projects" :key="project.id" class="project">
+        <img :src="project.image" :alt="project.title" />
         <div>
-          <h3>Lumina</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <button>demo</button>
-          <button>github</button>
+          <h3>{{ project.title }}</h3>
+          <p>{{ project.description }}</p>
+          <button @click="openLink(project.demoLink)">demo</button>
+          <button @click="openLink(project.githubLink)">github</button>
         </div>
       </article>
 
-      <article class="project">
-        <img src="/src/assets/projects/bookhaus-preview.png" />
-        <div>
-          <h3>Book Haus</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <button>demo</button>
-          <button>github</button>
-        </div>
-      </article>
-
-      <article class="project">
-        <img src="/src/assets/projects/webshop-preview.png" />
-        <div>
-          <h3>Webshop</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <button>demo</button>
-          <button>github</button>
-        </div>
-      </article>
-
-      <article class="project">
-        <img src="/src/assets/projects/quiz-preview.png" />
-        <div>
-          <h3>Quiz</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <button>demo</button>
-          <button>github</button>
-        </div>
-      </article>
-
-      <article class="project">
-        <img src="/src/assets/projects/nemo-preview.png" />
-        <div>
-          <h3>Nemo (UX)</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <button>demo</button>
-          <button>github</button>
-        </div>
-      </article>
+      <img class="tiny-star-graphic" src="../components/icons/tiny-star.png" />
     </section>
 
     <section class="creative-work-section">
@@ -159,13 +112,11 @@
           sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
         </p>
+        <img class="star-graphic" src="../components/icons/bigger-star.png" />
       </article>
 
-      <article class="artwork-section">
-        <img class="artwork" src="/src/assets/creative-work/paranoid-orange.JPG" />
-        <img class="artwork" src="/src/assets/creative-work/sad-blue.JPG" />
-        <img class="artwork" src="/src/assets/creative-work/tears-white.JPG" />
-        <img class="artwork" src="/src/assets/creative-work/self-portrait.JPG" />
+      <article v-for="artwork in artworks" :key="artwork.id" class="artwork-section">
+        <img class="artwork" :src="artwork.image" :alt="artwork.alt" />
       </article>
     </section>
   </main>
@@ -186,15 +137,33 @@ main {
   img {
     width: 500px;
   }
-  margin: 125px;
+}
+
+section {
+  margin: 0 125px;
 }
 
 .about-section {
   display: flex;
+  position: relative;
   align-items: flex-start;
   gap: 5rem;
   border-bottom: 1px solid white;
   border-top: 1px solid white;
+
+  .star-graphic {
+    position: absolute;
+    width: 100px;
+    bottom: 300px;
+    left: 30px;
+  }
+
+  .tiny-star-graphic {
+    position: absolute;
+    width: 40px;
+    bottom: 600px;
+    left: 30px;
+  }
 
   .wrapper {
     display: flex;
@@ -202,14 +171,27 @@ main {
     gap: 1rem;
   }
 
+  .portrait {
+    position: relative;
+    margin: 5rem 1rem;
+  }
+
   .technical-text {
     display: flex;
     flex-direction: row;
     gap: 18rem;
   }
+  .lines-graphic {
+    position: absolute;
+    width: 1000px;
+    bottom: -130px;
+    left: 920px;
+    z-index: 10;
+  }
 }
 
 .project-section {
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: 5rem;
@@ -233,17 +215,24 @@ main {
       margin-right: 1rem;
     }
   }
+
+  .tiny-star-graphic {
+    width: 40px;
+    position: absolute;
+    right: 125px;
+    bottom: 7rem;
+    z-index: 10;
+  }
 }
 
 .creative-work-section {
   display: flex;
   align-items: flex-start;
+  align-items: center;
   gap: 5rem;
-  margin: 7rem 0;
   text-align: center;
-  h2 {
-    margin: 0;
-  }
+  margin-top: 5rem;
+  margin-bottom: 6rem;
 }
 
 .artwork-section {
@@ -254,5 +243,12 @@ main {
   .artwork {
     width: 200px;
   }
+}
+
+.star-graphic {
+  display: block;
+  margin-left: -5rem;
+  margin-right: auto;
+  width: auto;
 }
 </style>
