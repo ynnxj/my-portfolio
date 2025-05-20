1d1ae641f930870a4e5d9482a822063f8b390827
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-20 15:04:22 +0200

    ✨ feat(navbar): add click function and handle scroll

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-const scrollToSection = () => {
-  const el = document.getElementById('about-section')
-  if (el) {
-    el.scrollIntoView({ behavior: 'smooth' })
+const router = useRouter()
+const route = useRoute()
+
+/**
+ * Handle click function to routerlinks in nav.
+ * Smooth scroll behaviour.
+ */
+const handleAboutClick = async () => {
+  if (route.path !== '/') {
+    await router.push('/')
   }
+
+  requestAnimationFrame(() => {
+    const el = document.getElementById('about-section')
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' })
+    }
+  })
 }
 
+/** Handle scroll function.
+ * Adds and removes scrolled-class to element.
+ */
 const handleScroll = () => {
   const navbar = document.getElementById('navbar')
   if (!navbar) return
@@ -33,23 +50,28 @@ onUnmounted(() => {
 </script>
 
 <template>
+  <!-- Navbar w/ routerlinks -->
   <nav id="navbar">
-    <img src="./icons/star-decal.svg" />
+    <RouterLink class="nav-icon-link" to="/">
+      <img class="nav-icon" src="./icons/star-decal.svg" alt="A white star icon." />
+    </RouterLink>
     <span>
       <RouterLink class="nav-link" to="/">home</RouterLink>
-      <RouterLink class="nav-link" to="/" @click.prevent="scrollToSection">about</RouterLink>
+      <a class="nav-link" href="#about-section" @click.prevent="handleAboutClick">about</a>
       <RouterLink class="nav-link" to="/contact">contact</RouterLink>
     </span>
   </nav>
 </template>
 
 <style lang="scss">
+@import '@/assets/variables';
+
 nav {
   position: fixed;
   width: 100vw;
+  height: 70px;
   top: 0;
   left: 0;
-  height: 70px;
   display: flex;
   align-items: center;
   gap: 50px;
@@ -61,15 +83,26 @@ nav {
     background-color: rgba(0, 0, 0, 0.8);
   }
 
-  img {
+  .nav-icon-link {
+    display: inline-block;
     width: 50px;
+    height: 50px;
     margin: 2rem;
   }
 
-  span {
-    .nav-link {
-      color: var(--primary-color);
-      margin-right: 80px;
+  .nav-icon {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  .nav-link {
+    margin-right: 80px;
+    color: $primary-color;
+    letter-spacing: 3px;
+
+    &:hover {
+      color: $accent-color;
     }
   }
 }
