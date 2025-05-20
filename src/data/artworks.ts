0142a3c4b3093d24be8f994c7a8714d046df48b0
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-20 15:04:49 +0200

    ✨ feat(data): add alt-text to artworks

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -2,21 +2,29 @@ export const artworks = [
   {
     id: 1,
     image: '/src/assets/creative-work/paranoid-orange.JPG',
-    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    alt: 'A painting of multiple orange colored faces, each with a paranoid facial expression and purple background.',
+    width: 410,
+    height: 590,
   },
   {
     id: 2,
     image: '/src/assets/creative-work/sad-blue.JPG',
-    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    alt: 'A painting of multiple blue faces, each with a sad facial expression, and a red background.',
+    width: 410,
+    height: 590,
   },
   {
     id: 3,
     image: '/src/assets/creative-work/tears-white.JPG',
-    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    alt: 'A painting of multiple white stars with sad faces, and a black background',
+    width: 410,
+    height: 590,
   },
   {
     id: 4,
     image: '/src/assets/creative-work/self-portrait.JPG',
-    alt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    alt: 'A portrait of a woman with long, dark hair and bangs. Face is covered a mischievous smiley.',
+    width: 410,
+    height: 590,
   },
 ]
