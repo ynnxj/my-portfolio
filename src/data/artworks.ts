b1d765ec9756d4058c9fa5e75e92204c0e639a29
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-05-20 17:40:15 +0200

    🐛 bug(data): change image path

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -1,28 +1,30 @@
-export const artworks = [
+import type { Artwork } from '@/types/Artwork'
+
+export const artworks: Artwork[] = [
   {
     id: 1,
-    image: '/src/assets/creative-work/paranoid-orange.JPG',
+    image: import.meta.env.BASE_URL + 'assets/creative-work/paranoid-orange.JPG',
     alt: 'A painting of multiple orange colored faces, each with a paranoid facial expression and purple background.',
     width: 410,
     height: 590,
   },
   {
     id: 2,
-    image: '/src/assets/creative-work/sad-blue.JPG',
+    image: import.meta.env.BASE_URL + 'assets/creative-work/sad-blue.JPG',
     alt: 'A painting of multiple blue faces, each with a sad facial expression, and a red background.',
     width: 410,
     height: 590,
   },
   {
     id: 3,
-    image: '/src/assets/creative-work/tears-white.JPG',
+    image: import.meta.env.BASE_URL + 'assets/creative-work/tears-white.JPG',
     alt: 'A painting of multiple white stars with sad faces, and a black background',
     width: 410,
     height: 590,
   },
   {
     id: 4,
-    image: '/src/assets/creative-work/self-portrait.JPG',
+    image: import.meta.env.BASE_URL + 'assets/creative-work/self-portrait.JPG',
     alt: 'A portrait of a woman with long, dark hair and bangs. Face is covered a mischievous smiley.',
     width: 410,
     height: 590,
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,10 @@
-export const projects = [
+import type { Project } from '@/types/Project'
+
+export const projects: Project[] = [
   {
     id: 1,
     title: 'Lumina',
-    image: '/src/assets/projects/lumina-preview.png',
+    image: import.meta.env.BASE_URL + 'assets/projects/lumina-preview.png',
     description:
       'A group assigment where we recieved a Figma design from another group, and turned it into a real app using Vue.js. This was my first time tackling a bigger Vue project, and I certainly learned a lot about building components and collaborating with others!',
     demoLink: 'https://ynnxj.github.io/lumina/',
@@ -11,7 +13,7 @@ export const projects = [
   {
     id: 2,
     title: 'Book Haus',
-    image: '/src/assets/projects/bookhaus-preview.png',
+    image: import.meta.env.BASE_URL + 'assets/projects/bookhaus-preview.png',
     description:
       'This was my first experience working with backend during an API course. My group and I created databases, and I was responsible for implementing the user authentication system, including login, logout, and registration. Connecting backend with frontend was challenging but very rewarding, for sure.',
     demoLink: '#',
@@ -20,7 +22,7 @@ export const projects = [
   {
     id: 3,
     title: 'Webshop',
-    image: '/src/assets/projects/webshop-preview.png',
+    image: import.meta.env.BASE_URL + 'assets/projects/webshop-preview.png',
     description:
       'My first project using JavaScript! I built a simple webshop and learned how to work with functions, events, and DOM manipulation. It was definitely a challenge to keep everything organized at first, but it helped me understand how JavaScript, HTML, and CSS come together to build interactive websites. This project gave me a solid foundation to keep evolving.',
     demoLink: 'https://ynnxj.github.io/webshop-assigment/',
@@ -29,7 +31,7 @@ export const projects = [
   {
     id: 4,
     title: 'Quiz',
-    image: '/src/assets/projects/quiz-preview.png',
+    image: import.meta.env.BASE_URL + 'assets/projects/quiz-preview.png',
     description:
       'Our first group assignment using JavaScript — and my first experience working in a dev team. We followed the Scrum method and used a modular structure to build the app. It was a great introduction to collaborative coding, task splitting, and version control.',
     demoLink: 'https://ynnxj.github.io/quiz-web-app/',
@@ -38,7 +40,7 @@ export const projects = [
   {
     id: 5,
     title: 'Nemo (UX)',
-    image: '/src/assets/projects/nemo-preview.png',
+    image: import.meta.env.BASE_URL + 'assets/projects/nemo-preview.png',
     description:
       'Taking what we learned in our UX course, my group designed this app. We came up with an idé, focused on wireframes, visual layout, page structure, fonts, and overall branding. The result was a complete design handoff and a clear guide for the other group to implement. This project helped me understand how to think from both a user and developer perspective during the design process.',
     demoLink: 'https://medieinstitutet.github.io/fed24d-grafiska-verktyg-clownfiskarna/',
