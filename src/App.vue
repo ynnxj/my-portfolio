f772561864adc892e3fbc1618a8faed94d0f3c31
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-06-08 17:27:56 +0200

    🔥  chore(clean up): remove and update code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import TheNavbar from './components/TheNavbar.vue'
-import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
   <div id="app">
     <TheNavbar />
     <RouterView />
-    <TheFooter />
   </div>
 </template>
